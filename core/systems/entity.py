8c93f00a98529267f7e5dd84188a01870b3934ad
Grapes <88650896+GrapesGoober@users.noreply.github.com>
2025-04-14 14:17:57 +0700

    #1 refactor core systems

--- core/systems/entity.py
+++ core/systems/entity.py
@@ -1,50 +1,55 @@
 """
-Entity system for game development ECS architecture. An Entity composes of components, which are just dataclass 
-for systems to run algorithms on. Component's lifecycle is determined by parent Entity. 
+A simplified ECS architecture for game development. An Entity composes of components,
+which are just dataclass for systems to run algorithms on. Component's lifecycle is
+determined by its parent Entity.
 """
 
+
 class Component:
-    """ A component is an informal interface for lifecycle `on_add` and `on_remove` methods. """
-    def on_add(self): ...
-    def on_remove(self): ...
+    """
+    Informal interface for lifecycle `on_add` and `on_remove` methods, called
+    by `Scene` to reflect entity's lifcycle. Component instances must not be
+    shared across entities.
+    """
+
+    def on_add(self) -> None: ...
+    def on_remove(self) -> None: ...
+
 
 class Entity:
-    """ An Entity base class. Entity can hold logic, data, and components.
-        This base class is meant to be inherited to create custom entity classes. """
+    """
+    An ECS Entity can hold logic, data, and components. Inherit this base class to create custom
+    entity classes. Entities don't interact directly with each other, only through `Event`.
+    """
 
     def __init__(self, *components: Component) -> None:
-        self.components: list[Component] = list(components)
+        self._components: list[Component] = list(components)
 
-    def add_component(self, component: Component) -> None:
-        """ Assigns a new component to this entity. The lifetime of this component is matched tot this entity. """
-        if component not in self.components:
-            self.components.append(component)
-            if self in Entities.entities:
-                component.on_add()
-    
-    def remove_component(self, component: Component) -> None:
-        if component in self.components:
-            self.components.remove(component)
-            if self in Entities.entities:
-                component.on_remove()
+    @property
+    def components(self) -> list[Component]:
+        """A read-only components list. Once initialized, entity can't change this list."""
+        return self._components
+
 
 class Entities:
-    """ An Entity system pool. Manages lifetimes for entities & their components. """
-    entities: list['Entity'] = []
+    """An Entity pool managing entities, their componenents, & lifecycle"""
+
+    _entities: set[Entity] = set([])
 
     @staticmethod
     def add(*entities: Entity) -> None:
-        for g in entities:
-            if g not in Entities.entities:
-                Entities.entities.append(g)
-                for c in g.components:
-                    c.on_add()
-                    
+        """Adds a new entity to the game. Has no effect if entity is already added."""
+        for entity in entities:
+            if entity not in Entities._entities:
+                Entities._entities.add(entity)
+                for component in entity.components:
+                    component.on_add()
+
     @staticmethod
     def remove(*entities: Entity) -> None:
-        for g in entities:
-            if g in Entities.entities:
-                Entities.entities.remove(g)
-                for c in g.components:
-                    c.on_remove()
-
+        """Adds a new entity to the game. Has no effect if entity is not present."""
+        for entity in entities:
+            if entity in Entities._entities:
+                Entities._entities.remove(entity)
+                for component in entity.components:
+                    component.on_remove()
