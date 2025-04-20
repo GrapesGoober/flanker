735878dacca240cc6c83d3194b78a9a28b118ce9
Grapes <88650896+GrapesGoober@users.noreply.github.com>
2025-04-20 08:22:20 +0700

    #12 fix move controls intersect logic

--- core/move_controls.py
+++ core/move_controls.py
@@ -5,20 +5,24 @@ from los_check import LosChecker
 from terrain_types import TerrainFlag
 from vec2 import Vec2
 
+_MOVE_COMPONENTS = Transform, MovementControls, UnitCondition
+_MOVE_COMPONENTS_T = tuple[Transform, MovementControls, UnitCondition]
+
+
+def get_components(unit_id: int) -> _MOVE_COMPONENTS_T | None:
+    return esper.try_components(unit_id, *_MOVE_COMPONENTS)
+
 
 class MoveControls:
     @staticmethod
     def move(unit_id: int, to: Vec2) -> None:
 
-        # Check move action is valid
-        if not (
-            comps := esper.try_components(
-                unit_id, Transform, MovementControls, UnitCondition
-            )
-        ):
+        # Check component dependency
+        if not (comps := get_components(unit_id)):
             return
         transform, _, cond = comps
 
+        # Check move action is valid
         if cond.status != UnitCondition.Status.ACTIVE:
             return
         for intersect in Intersects.get(transform.position, to):
@@ -29,12 +33,12 @@ class MoveControls:
         STEP_SIZE = 0.1
         length = (to - transform.position).length()
         direction = (to - transform.position).normalized()
-        for step in range(int(length / STEP_SIZE) - 1):
+        for _ in range(int(length / STEP_SIZE) + 1):
             transform.position += direction * STEP_SIZE
             is_spotted = LosChecker.is_spotted(unit_id)
             if is_spotted:  # Spotted, stop right there
                 cond.status = UnitCondition.status.SUPPRESSED
                 return
 
-        # Not spotted, move to destination
+        # # Not spotted, move to destination
         transform.position = to
