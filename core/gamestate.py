cfce921b30212044982587f095a0f72572ca02be
Grapes <88650896+GrapesGoober@users.noreply.github.com>
2025-05-02 22:09:59 +0700

    #18 add docstrings

--- core/gamestate.py
+++ core/gamestate.py
@@ -2,12 +2,16 @@ from typing import Any, Iterable
 
 
 class GameState:
+    """Encapsulates ECS entities & components into a game state."""
+
     def __init__(self) -> None:
+        """Initializes the game state with empty entities."""
         self._id_counter: int = 0
         self._entities: dict[int, dict[type[Any], Any]] = {}
         self._cache: dict[type, list[tuple[int, Any]]] = {}
 
     def add_entity(self, *components: Any) -> int:
+        """Adds a new entity with the given components, returns ID."""
         entity_id = self._id_counter
         self._id_counter += 1
         self._entities[entity_id] = {type(c): c for c in components}
@@ -15,13 +19,16 @@ class GameState:
         return entity_id
 
     def delete_entity(self, entity_id: int) -> None:
+        """Deletes an entity by its ID"""
         self._entities.pop(entity_id)
         self._cache = {}
 
     def get_component[T](self, entity_id: int, component_type: type[T]) -> T | None:
+        """Get an entity's component. None if entity or component not found."""
         return self._entities.get(entity_id, {}).get(component_type)
 
     def get_entities[T](self, component_type: type[T]) -> Iterable[tuple[int, T]]:
+        """Yields all entities with a specific component type."""
         if component_type in self._cache:
             yield from self._cache[component_type]
         else:
