2c816f9cfce9341bb90c3bba20091ca6813a21f0
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-06-06 21:02:22 +0700

    #15 add move test cases

--- core/tests/test_reactive_fire.py
+++ core/tests/test_reactive_fire.py
@@ -17,8 +17,11 @@ class Fixture:
 @pytest.fixture
 def fixture() -> Fixture:
     gs = GameState()
-    # Rifle Squad
+    # Rifle Squads
     cmd = gs.add_entity(CommandUnit())
+    gs.add_entity(
+        MoveControls(), CombatUnit(command_id=cmd), Transform(position=Vec2(15, 20))
+    )
     id = gs.add_entity(
         MoveControls(), CombatUnit(command_id=cmd), Transform(position=Vec2(0, -10))
     )
@@ -36,20 +39,6 @@ def fixture() -> Fixture:
         ),
     )
 
-    # 10x10 opaque box
-    gs.add_entity(
-        TerrainFeature(
-            vertices=[
-                Vec2(0, 0),
-                Vec2(-10, 0),
-                Vec2(-10, -10),
-                Vec2(0, -10),
-                Vec2(0, 0),
-            ],
-            flag=TerrainFeature.Flag.WALKABLE,
-        ),
-    )
-
     return Fixture(gs, id)
 
 
@@ -58,12 +47,16 @@ def test_move(fixture: Fixture) -> None:
     transform = fixture.gs.get_component(fixture.unit_id, Transform)
     assert transform and (
         transform.position == Vec2(5, -15)
-    ), "Unit expects at Vec2(5, -15)"
+    ), "Target expects at Vec2(5, -15)"
 
 
-def test_move_invalid(fixture: Fixture) -> None:
-    MoveAction.move(fixture.gs, fixture.unit_id, Vec2(6, 6))
+def test_los_interrupt(fixture: Fixture) -> None:
+    MoveAction.move(fixture.gs, fixture.unit_id, Vec2(20, -10))
     transform = fixture.gs.get_component(fixture.unit_id, Transform)
     assert transform and (
-        transform.position == Vec2(0, -10)
-    ), "Unit expects to not move"
+        transform.position == Vec2(7.6, -10)
+    ), "Target expects at Vec2(7.6, -10)"
+    unit = fixture.gs.get_component(fixture.unit_id, CombatUnit)
+    assert unit and (
+        unit.status == CombatUnit.status.SUPPRESSED
+    ), "Target expects to be suppressed"
