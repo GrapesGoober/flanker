cae4ef4aae61da38c310bf786c8106a8768435f2
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-06-09 21:30:34 +0700

    #39 cleans up code

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import * as d3 from 'd3';
 	import {
 		GetTerrainData,
 		GetRifleSquadsData,
-		MoveRifleSquad,
 		type RifleSquadData,
-		type TerrainFeatureData,
-		type Vec2
+		type TerrainFeatureData
 	} from '$lib';
 	import Map from '$lib/map.svelte';
 
@@ -21,27 +18,9 @@
 		unitData: []
 	});
 
-	let svgLayer: SVGSVGElement | null = $state(null);
-	let zoomLayer: SVGGElement | null = $state(null);
-	let transform: d3.ZoomTransform = $state(d3.zoomIdentity);
-	let marker: Vec2 | null = $state(null);
-	let selectedUnit: number | null = $state(null);
-
 	onMount(async () => {
 		mapProps.terrainData = await GetTerrainData();
 		mapProps.unitData = await GetRifleSquadsData();
-
-		const svg = d3.select(svgLayer);
-		const g = d3.select(zoomLayer);
-		const zoom = d3
-			.zoom<SVGSVGElement, unknown>()
-			.scaleExtent([0.5, 10])
-			.on('zoom', (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
-				transform = event.transform;
-				g.attr('transform', transform.toString());
-			});
-
-		svg.call(zoom as any);
 	});
 </script>
 
