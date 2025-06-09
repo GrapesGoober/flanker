a33a6294b39f8bf42d25e7ce6ac8c37b14318300
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-06-09 22:18:17 +0700

    #39 refactor svg map component

---
+++ frontend/src/lib/svg-map.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { onMount, type Snippet } from 'svelte';
+	import * as d3 from 'd3';
+	import type { Vec2 } from '$lib';
+
+	type Props = {
+		onclick: (event: MouseEvent, worldPos: Vec2) => void;
+		body: Snippet;
+	};
+	let props: Props = $props();
+	let svgLayer: SVGSVGElement | null = null;
+	let zoomLayer: SVGGElement | null = null;
+	let transform: d3.ZoomTransform = d3.zoomIdentity;
+
+	onMount(() => {
+		const svg = d3.select(svgLayer);
+		const g = d3.select(zoomLayer);
+		const zoom = d3
+			.zoom<SVGSVGElement, unknown>()
+			.scaleExtent([0.5, 10])
+			.on('zoom', (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
+				transform = event.transform;
+				g.attr('transform', transform.toString());
+			});
+
+		svg.call(zoom as any);
+	});
+
+	function onclick(event: MouseEvent) {
+		// Convert click event to world position vector
+		const point = d3.pointer(event, svgLayer); // Get click coords in SVG space
+		const worldPos = transform.invert(point); // Adjust for zoom/pan to world space
+		const pos: Vec2 = { x: worldPos[0], y: worldPos[1] };
+		props.onclick(event, pos);
+	}
+</script>
+
+<!-- I'm prototying behaviours at the moment, so proper structure comes later -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<svg bind:this={svgLayer} width="100%" height="90vh" style="border: 1px solid #ccc" {onclick}>
+	<g bind:this={zoomLayer}>
+		{@render props.body()}
+	</g>
+</svg>
+
+<style>
+	svg {
+		touch-action: none; /* required for pointer-based zooming */
+		background-color: #fefae0;
+	}
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,25 +3,69 @@
 	import {
 		GetTerrainData,
 		GetRifleSquadsData,
+		MoveRifleSquad,
 		type RifleSquadData,
-		type TerrainFeatureData
+		type TerrainFeatureData,
+		type Vec2
 	} from '$lib';
-	import Map from '$lib/map.svelte';
+	import RifleSquad from '$lib/rifle-squad.svelte';
+	import TerrainFeature from '$lib/terrain-feature.svelte';
+	import SvgMap from '$lib/svg-map.svelte';
 
-	type MapProps = {
-		terrainData: TerrainFeatureData[];
-		unitData: RifleSquadData[];
-	};
-
-	let mapProps: MapProps = $state({
-		terrainData: [],
-		unitData: []
-	});
+	let marker: Vec2 | null = $state(null);
+	let terrainData: TerrainFeatureData[] = $state([]);
+	let unitData: RifleSquadData[] = $state([]);
+	let selectedUnit: number | null = $state(null);
 
 	onMount(async () => {
-		mapProps.terrainData = await GetTerrainData();
-		mapProps.unitData = await GetRifleSquadsData();
+		terrainData = await GetTerrainData();
+		unitData = await GetRifleSquadsData();
 	});
+
+	function AddMarker(event: MouseEvent, worldPos: Vec2) {
+		if (selectedUnit === null) {
+			return;
+		}
+		marker = worldPos;
+	}
+
+	async function ConfirmMarker(_: MouseEvent) {
+		// Only apply marker for selected squad & existing marker
+		if (selectedUnit !== null && marker !== null) {
+			unitData = await MoveRifleSquad(selectedUnit, marker);
+			marker = null;
+		}
+	}
+
+	function SelectUnit(unit_id: number, event: MouseEvent) {
+		event.stopPropagation();
+		// Only select unit when no marker is active
+		if (marker !== null) {
+			return;
+		}
+		selectedUnit = unit_id;
+	}
 </script>
 
-<Map bind:props={mapProps}></Map>
+<!-- I'm prototying behaviours at the moment, so proper structure comes later -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+{#snippet mapMarkup()}
+	{#each terrainData as terrainFeatureData}
+		<TerrainFeature featureData={terrainFeatureData} />
+	{/each}
+
+	{#each unitData as unit}
+		<g onclick={(event) => SelectUnit(unit.unit_id, event)}>
+			<RifleSquad position={unit.position} isSelected={selectedUnit === unit.unit_id} />
+		</g>
+	{/each}
+
+	{#if marker}
+		<g onclick={ConfirmMarker}>
+			<circle cx={marker.x} cy={marker.y} r="10" fill="red" />
+		</g>
+	{/if}
+{/snippet}
+
+<SvgMap onclick={AddMarker} body={mapMarkup} />
