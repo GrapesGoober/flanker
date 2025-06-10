f7b62bfb2b3fce5082b12077615fff237cc6d86c
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-06-10 20:46:16 +0700

    #39 add basic editor controls

--- frontend/src/routes/editor/+page.svelte
+++ frontend/src/routes/editor/+page.svelte
@@ -1,27 +1,34 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import {
-		GetTerrainData,
-		GetRifleSquadsData,
-		type RifleSquadData,
-		type TerrainFeatureData
-	} from '$lib';
-	import Map from '$lib/map.svelte';
+	import { GetTerrainData, GetRifleSquadsData, type TerrainFeatureData, type Vec2 } from '$lib';
+	import TerrainFeature from '$lib/terrain-feature.svelte';
+	import SvgMap from '$lib/svg-map.svelte';
 
-	type MapProps = {
-		terrainData: TerrainFeatureData[];
-		unitData: RifleSquadData[];
-	};
-
-	let mapProps: MapProps = $state({
-		terrainData: [],
-		unitData: []
-	});
+	let marker: Vec2 | null = $state(null);
+	let terrainData: TerrainFeatureData[] = $state([]);
 
 	onMount(async () => {
-		mapProps.terrainData = await GetTerrainData();
-		mapProps.unitData = await GetRifleSquadsData();
+		terrainData = await GetTerrainData();
 	});
+
+	function AddMarker(event: MouseEvent, worldPos: Vec2) {
+		marker = worldPos;
+	}
 </script>
 
-<Map bind:props={mapProps}></Map>
+<!-- I'm prototying behaviours at the moment, so proper structure comes later -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+{#snippet mapMarkup()}
+	{#each terrainData as terrainFeatureData}
+		<TerrainFeature featureData={terrainFeatureData} />
+	{/each}
+
+	{#if marker}
+		<g>
+			<circle cx={marker.x} cy={marker.y} r="10" fill="red" />
+		</g>
+	{/if}
+{/snippet}
+
+<SvgMap onclick={AddMarker} body={mapMarkup} />
