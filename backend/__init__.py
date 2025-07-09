4bbfa65f672c1c5bb3b8789cf0cd5f75ec44c799
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-07-09 10:29:15 +0700

    #51 add backend API faction validation

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 from backend.basic_ai import BasicAi
 from backend.scene import new_scene
@@ -8,6 +8,7 @@ from backend.models import (
 )
 from backend.squad import UnitStateController, UnitState
 from backend.terrain import TerrainController
+from core.command import Command
 from core.move_action import MoveAction
 
 context = new_scene()
@@ -21,6 +22,11 @@ async def get_rifle_squads() -> UnitState:
 
 @app.post("/api/move")
 async def action_move(body: MoveActionRequest) -> UnitState:
+    if Command.get_faction_id(context.gs, body.unit_id) != context.player_faction_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Unit {body.unit_id} is not part of player faction",
+        )
     MoveAction.move(context.gs, body.unit_id, body.to)
     BasicAi.play(context.gs, context.opponent_faction_id)
     return UnitStateController.get_unit_state(context.gs, context.player_faction_id)
