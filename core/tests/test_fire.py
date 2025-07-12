76aaf17705ceca5cf42e11bff7416cbe74496e0f
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-07-12 10:16:26 +0700

    #10 add fire action tests for each outcome

--- core/tests/test_fire.py
+++ core/tests/test_fire.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
 import pytest
 
-from core.components import Faction, FireControls, TerrainFeature, CombatUnit, Transform
+from core.components import (
+    Faction,
+    FireControls,
+    TerrainFeature,
+    CombatUnit,
+    Transform,
+)
 from core.fire_action import FireAction
 from core.gamestate import GameState
 from core.vec2 import Vec2
@@ -12,20 +18,24 @@ class Fixture:
     gs: GameState
     attacker_id: int
     target_id: int
+    fire_controls: FireControls
 
 
 @pytest.fixture
 def fixture() -> Fixture:
     gs = GameState()
     # Rifle Squads
-    faction = gs.add_entity(Faction(has_initiative=True))
+    faction = gs.add_entity(
+        Faction(has_initiative=True),
+    )
     attacker_id = gs.add_entity(
         CombatUnit(command_id=faction),
-        FireControls(override=FireControls.Outcomes.SUPPRESS),
-        Transform(position=Vec2(0, -10)),
+        fire_controls := FireControls(),
+        Transform(position=Vec2(7.6, -10)),
     )
     target_id = gs.add_entity(
-        CombatUnit(command_id=faction), Transform(position=Vec2(15, 20))
+        CombatUnit(command_id=faction),
+        Transform(position=Vec2(15, 20)),
     )
     # 10x10 opaque box
     gs.add_entity(
@@ -45,23 +55,66 @@ def fixture() -> Fixture:
         gs=gs,
         attacker_id=attacker_id,
         target_id=target_id,
+        fire_controls=fire_controls,
     )
 
 
-def test_no_fire(fixture: Fixture) -> None:
-    FireAction.fire(fixture.gs, fixture.attacker_id, fixture.target_id)
+def test_no_los(fixture: Fixture) -> None:
+    # Set attacker to position that is obstructed
+    attacker_transform = fixture.gs.get_component(
+        fixture.attacker_id,
+        Transform,
+    )
+    assert attacker_transform
+    attacker_transform.position = Vec2(0, -10)
+    # Fire action won't occur
+    fire_result = FireAction.fire(
+        fixture.gs,
+        fixture.attacker_id,
+        fixture.target_id,
+    )
+    assert fire_result == False, "Fire action mustn't occur"
     target = fixture.gs.get_component(fixture.target_id, CombatUnit)
     assert target and (
         target.status == CombatUnit.Status.ACTIVE
-    ), "Target target expects to be ACTIVE as it is obstructed"
+    ), "Target expects to be ACTIVE as it is obstructed"
 
 
-def test_fire(fixture: Fixture) -> None:
-    attacker_transform = fixture.gs.get_component(fixture.attacker_id, Transform)
-    assert attacker_transform
-    attacker_transform.position = Vec2(7.6, -10)
-    FireAction.fire(fixture.gs, fixture.attacker_id, fixture.target_id)
+def test_no_fire(fixture: Fixture) -> None:
+    fixture.fire_controls.override = FireControls.Outcomes.MISS
+    fire_result = FireAction.fire(
+        fixture.gs,
+        fixture.attacker_id,
+        fixture.target_id,
+    )
+    assert fire_result == True, "Fire action must occur"
+    target = fixture.gs.get_component(fixture.target_id, CombatUnit)
+    assert target and (
+        target.status == CombatUnit.Status.ACTIVE
+    ), "Target expects to be ACTIVE as fire action MISS"
+
+
+def test_suppress_fire(fixture: Fixture) -> None:
+    fixture.fire_controls.override = FireControls.Outcomes.SUPPRESS
+    fire_result = FireAction.fire(
+        fixture.gs,
+        fixture.attacker_id,
+        fixture.target_id,
+    )
+    assert fire_result == True, "Fire action must occur"
     target = fixture.gs.get_component(fixture.target_id, CombatUnit)
     assert target and (
         target.status == CombatUnit.Status.SUPPRESSED
-    ), "Target target expects to be SUPPRESSED as it is shot at"
+    ), "Target expects to be SUPPRESSED as it is shot"
+
+
+def test_kill_fire(fixture: Fixture) -> None:
+    fixture.fire_controls.override = FireControls.Outcomes.KILL
+    fire_result = FireAction.fire(
+        fixture.gs,
+        fixture.attacker_id,
+        fixture.target_id,
+    )
+    assert fire_result == True, "Fire action must occur"
+    target = fixture.gs.get_component(fixture.target_id, CombatUnit)
+    assert target == None, "Target expects to be KILLED as it is shot"
