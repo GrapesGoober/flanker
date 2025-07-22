391bf91fd468459618f929ce42452e97d2940eed
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-07-22 14:48:48 +0700

    #40 add arrow function

--- frontend/src/lib/svg-icons/arrow.svelte
+++ frontend/src/lib/svg-icons/arrow.svelte
@@ -1,51 +1,65 @@
 <script lang="ts">
 	import type { Vec2 } from '$lib';
+	import * as d3 from 'd3';
 
 	type Props = {
 		start: Vec2;
 		end: Vec2;
 	};
 	let props: Props = $props();
-	const offset = 10;
-	const dx = props.end.x - props.start.x;
-	const dy = props.end.y - props.start.y;
-	const len = Math.hypot(dx, dy);
-	const factor = (len - offset) / len;
-	let shortenLine: Vec2 = {
-		x: props.start.x + dx * factor,
-		y: props.start.y + dy * factor
-	};
+
+	const headLength = 5;
+	const headWidth = 3;
+	function GetPoints() {
+		// Normalized directional vector of this arrow
+		const dx = props.end.x - props.start.x;
+		const dy = props.end.y - props.start.y;
+		const length = Math.hypot(dx, dy);
+		const ux = dx / length;
+		const uy = dy / length;
+
+		// Point before arrowhead starts
+		const shaftEnd: Vec2 = {
+			x: props.end.x - ux * headLength,
+			y: props.end.y - uy * headLength
+		};
+
+		// Perpendicular vector for head width
+		const perpX = -uy;
+		const perpY = ux;
+
+		// Arrowhead points (triangle)
+		const leftHead: Vec2 = {
+			x: shaftEnd.x + perpX * headWidth,
+			y: shaftEnd.y + perpY * headWidth
+		};
+		const rightHead: Vec2 = {
+			x: shaftEnd.x - perpX * headWidth,
+			y: shaftEnd.y - perpY * headWidth
+		};
+
+		return [props.start, shaftEnd, leftHead, props.end, rightHead, shaftEnd];
+	}
+
+	function GetPath(coords: Vec2[]): string {
+		const line = d3
+			.line<Vec2>()
+			.x((d) => d.x)
+			.y((d) => d.y)
+			.curve(d3.curveCardinal.tension(1));
+		return line(coords) || '';
+	}
+
+	let path: string = $derived(GetPath(GetPoints()));
 </script>
 
-<defs>
-	<marker
-		id="arrow"
-		viewBox="0 0 6 6"
-		refX="0"
-		refY="3"
-		markerWidth="3"
-		markerHeight="3"
-		orient="auto-start-reverse"
-	>
-		<path d="M 0 0 L 6 3 L 0 6 z" />
-	</marker>
-</defs>
-
-<line
-	x1={props.start.x}
-	y1={props.start.y}
-	x2={shortenLine.x}
-	y2={shortenLine.y}
-	marker-end="url(#arrow)"
-/>
+<path d={path} />
 
 <style lang="less">
-	@color: rgba(0, 0, 0);
-	line {
-		stroke: @color;
-		stroke-width: 3;
-	}
+	@color: rgba(0, 0, 0, 0.5);
 	path {
-		fill: @color;
+		fill: none;
+		stroke: @color;
+		stroke-width: 5;
 	}
 </style>
