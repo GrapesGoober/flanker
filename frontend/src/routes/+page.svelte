a325b208f0729c2b463dc368ec3f5e13777c8a45
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-07-23 10:24:31 +0700

    #40 refactor to state pattern player controller

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,14 +3,14 @@
 	import RifleSquad from '$lib/rifle-squad.svelte';
 	import SvgMap from '$lib/map/svg-map.svelte';
 	import Arrow from '$lib/svg-icons/arrow.svelte';
-	import { PlayerState } from './player-state.svelte';
+	import { PlayerController } from './player-state.svelte';
 
 	let map: SvgMap | null = $state(null);
 	let clickTarget: HTMLElement | null = $state(null);
-	let playerState: PlayerState = $state(new PlayerState());
+	let controller: PlayerController = $state(new PlayerController());
 
 	onMount(async () => {
-		await playerState.initializeAsync();
+		await controller.initializeAsync();
 	});
 
 	function AddMarker(event: MouseEvent) {
@@ -19,26 +19,26 @@
 		const rect = node.getBoundingClientRect();
 		const x = event.clientX - rect.x;
 		const y = event.clientY - rect.y;
-		playerState.addMoveMarker(map.ToWorldCoords({ x, y }));
+		controller.setMoveMarker(map.ToWorldCoords({ x, y }));
 	}
 
 	async function ConfirmMarker() {
-		await playerState.moveToMarkerAsync();
+		await controller.moveToMarkerAsync();
 	}
 
 	function SelectUnit(unitId: number, event: MouseEvent) {
 		event.stopPropagation();
-		playerState.selectUnit(unitId);
+		controller.selectUnit(unitId);
 	}
 
 	function CancleMarker() {
-		playerState.cancelMarker();
+		controller.cancelMarker();
 	}
 
 	async function OnKeyDown(event: KeyboardEvent) {
 		const key = event.key.toLowerCase();
-		if (key === 'c') playerState.cancelMarker();
-		if (key === 'm') await playerState.moveToMarkerAsync();
+		if (key === 'c') controller.cancelMarker();
+		if (key === 'm') await controller.moveToMarkerAsync();
 	}
 </script>
 
@@ -47,15 +47,20 @@
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 {#snippet mapSvgSnippet()}
-	{#if playerState.moveMarker && playerState.selectedUnit}
-		<circle cx={playerState.moveMarker.x} cy={playerState.moveMarker.y} r="5" class="move-circle" />
-		<Arrow start={playerState.selectedUnit.position} end={playerState.moveMarker} />
+	{#if controller.state.type == 'marked'}
+		<circle
+			cx={controller.state.moveMarker.x}
+			cy={controller.state.moveMarker.y}
+			r="5"
+			class="move-circle"
+		/>
+		<Arrow start={controller.state.selectedUnit.position} end={controller.state.moveMarker} />
 	{/if}
 
-	{#each playerState.unitData.squads as unit, index}
-		{#if playerState.unitData.squads[index]}
+	{#each controller.unitData.squads as unit, index}
+		{#if controller.unitData.squads[index]}
 			<g onclick={(event) => SelectUnit(unit.unitId, event)}>
-				<RifleSquad bind:rifleSquadData={playerState.unitData.squads[index]} />
+				<RifleSquad bind:rifleSquadData={controller.unitData.squads[index]} />
 			</g>
 		{/if}
 	{/each}
@@ -64,10 +69,10 @@
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div bind:this={clickTarget} onclick={AddMarker}>
-	<SvgMap svgSnippet={mapSvgSnippet} terrainData={playerState.terrainData} bind:this={map} />
+	<SvgMap svgSnippet={mapSvgSnippet} terrainData={controller.terrainData} bind:this={map} />
 </div>
 
-{#if playerState.moveMarker}
+{#if controller.state.type == 'marked'}
 	<div class="action-box">
 		<button onclick={CancleMarker} class="action-button">Cancel (c)</button>
 		<br /><br />
--- frontend/src/routes/player-state.svelte.ts
+++ frontend/src/routes/player-state.svelte.ts
@@ -8,48 +8,62 @@ import {
 	type Vec2
 } from '$lib';
 
-export class PlayerState {
+type PlayerControllerState =
+	| { type: 'default' }
+	| { type: 'selected'; selectedUnit: RifleSquadData }
+	| { type: 'marked'; selectedUnit: RifleSquadData; moveMarker: Vec2 };
+
+export class PlayerController {
 	terrainData: TerrainFeatureData[] = $state([]);
 	unitData: CombatUnitsData = $state({
 		hasInitiative: false,
 		squads: []
 	});
-	moveMarker: Vec2 | null = $state(null);
-	selectedUnit: RifleSquadData | null = $state(null);
+
+	state: PlayerControllerState = $state({ type: 'default' });
 
 	async initializeAsync() {
 		this.terrainData = await GetTerrainData();
 		this.unitData = await GetUnitStatesData();
 	}
 
-	addMoveMarker(at: Vec2) {
-		if (!this.unitData.hasInitiative) return;
-		if (this.selectedUnit === null) return;
-		this.moveMarker = at;
-	}
-
-	async moveToMarkerAsync() {
-		if (!this.unitData.hasInitiative) return;
-		if (this.selectedUnit === null) return;
-		if (this.moveMarker === null) return;
-
-		this.unitData = await MoveRifleSquad(this.selectedUnit.unitId, this.moveMarker);
-		this.moveMarker = null;
-		this.selectedUnit = null;
-	}
-
 	selectUnit(unitId: number) {
-		if (this.moveMarker != null) return;
+		if (this.state.type != 'default') return;
 
 		for (const unit of this.unitData.squads) {
 			unit.isSelected = unit.unitId === unitId;
 			if (unit.unitId === unitId && unit.isFriendly === true) {
-				this.selectedUnit = unit;
+				this.state = {
+					type: 'selected',
+					selectedUnit: unit
+				};
 			}
 		}
 	}
 
+	setMoveMarker(at: Vec2) {
+		if (!this.unitData.hasInitiative) return;
+		if (this.state.type == 'default') return;
+		this.state = {
+			type: 'marked',
+			moveMarker: at,
+			selectedUnit: this.state.selectedUnit
+		};
+	}
+
 	cancelMarker() {
-		this.moveMarker = null;
+		this.state = {
+			type: 'default'
+		};
+	}
+
+	async moveToMarkerAsync() {
+		if (!this.unitData.hasInitiative) return;
+		if (this.state.type != 'marked') return;
+
+		this.unitData = await MoveRifleSquad(this.state.selectedUnit.unitId, this.state.moveMarker);
+		this.state = {
+			type: 'default'
+		};
 	}
 }
