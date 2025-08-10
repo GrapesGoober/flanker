60804bd023d4928ef09a59f97ebaae201b9458a3
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-08-10 09:58:56 +0700

    #78 refactor +page to root of component hierarchy

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,9 +2,22 @@
 	import { onMount } from 'svelte';
 	import { PlayerController } from './player-controller.svelte';
 	import ControlPanel from './control-panel.svelte';
-	import GameplayMap from './gameplay-map.svelte';
+	import SvgMap from '$lib/map/svg-map.svelte';
+	import TerrainFeatures from './terrain-features.svelte';
+	import CombatUnits from './combat-units.svelte';
 
 	let controller: PlayerController = $state(new PlayerController());
+	let map: SvgMap | null = $state(null);
+	let clickTarget: HTMLElement | null = $state(null);
+
+	function AddMarker(event: MouseEvent) {
+		if (map == null) return;
+		const node = clickTarget as HTMLElement;
+		const rect = node.getBoundingClientRect();
+		const x = event.clientX - rect.x;
+		const y = event.clientY - rect.y;
+		controller.setMoveMarker(map.ToWorldCoords({ x, y }));
+	}
 
 	onMount(async () => {
 		await controller.initializeAsync();
@@ -17,5 +30,17 @@
 	});
 </script>
 
-<GameplayMap bind:controller />
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+{#snippet mapSvgSnippet()}
+	<TerrainFeatures terrainData={controller.terrainData} />
+	<CombatUnits {controller} />
+{/snippet}
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div bind:this={clickTarget} onclick={AddMarker}>
+	<SvgMap svgSnippet={mapSvgSnippet} bind:this={map} />
+</div>
+
 <ControlPanel bind:controller />
