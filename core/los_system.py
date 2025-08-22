02d2976ff70dfc507c3c9e75a6be1c3cbcbfdc8c
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-08-22 16:21:20 +0700

    #55 add parallel terrain filter array

--- core/los_system.py
+++ core/los_system.py
@@ -7,15 +7,14 @@ from numpy.typing import NDArray
 
 from core.utils.linear_transform import LinearTransform
 
-# TODO: this global cache is breaking tests,
-# running one test at a time solves this
-_cached_edges: dict[int, tuple[NDArray[np.float64], NDArray[np.float64]]] = {}
+# TODO: this global cache is breaking tests, opt to use game state or action level cache
+# This is a no-exclusion terrain data
+_cached_terrain: tuple[NDArray[np.float64], NDArray[np.float64]] | None = None
+_cached_edge_ids: list[int] = []
 
-# Technically, the entity-to-terrain shouldn't be cached
-# Instead, the caller (move action and fire action) should get source's terrain once
-# This cache is just to simulate that effect
-# TODO: move action batch all sources and targets
-_cached_source_ent: dict[int, int] = {}
+# TODO: move system should be responsible for tracking this is_inside
+# for each entity. Simulate for now using cache
+_terrain_filter: dict[int, NDArray[np.bool]] = {}
 
 
 class LosSystem:
@@ -31,55 +30,67 @@ class NewLosSystem:
         gs: GameState, source_ent: int
     ) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
 
-        if source_ent not in _cached_source_ent:
-            _cached_source_ent[source_ent] = -1
-            for id, terrain, _ in gs.query(TerrainFeature, Transform):
-                if terrain.flag & TerrainFeature.Flag.OPAQUE:
-                    if IntersectSystem.is_inside(gs, id, source_ent):
-                        _cached_source_ent[source_ent] = id
+        global _cached_terrain, _cached_edge_ids
+        if _cached_terrain != None:
+            return _cached_terrain
 
-        excluded_terrain_id = _cached_source_ent[source_ent]
+        edge_sources: list[NDArray[np.float64]] = []
+        edge_targets: list[NDArray[np.float64]] = []
+        _cached_edge_ids = []
+        for id, terrain, transform in gs.query(TerrainFeature, Transform):
+            if terrain.flag & TerrainFeature.Flag.OPAQUE:
+                vertices = LinearTransform.apply(terrain.vertices, transform)
+                # Explicitly tell numpy that we're working with 2d vectors with z=0
+                poly = np.array([[v.x, v.y, 0] for v in vertices], dtype=np.float64)
+                shifted_poly = np.roll(poly, shift=-1, axis=0)
+                edge_sources.append(poly)
+                edge_targets.append(shifted_poly)
+                _cached_edge_ids += [id for _ in vertices]
 
-        if excluded_terrain_id not in _cached_edges:
-            edge_sources: list[NDArray[np.float64]] = []
-            edge_targets: list[NDArray[np.float64]] = []
-            for id, terrain, transform in gs.query(TerrainFeature, Transform):
-                if terrain.flag & TerrainFeature.Flag.OPAQUE:
-                    vertices = LinearTransform.apply(terrain.vertices, transform)
-                    # Explicitly tell numpy that we're working with 2d vectors with z=0
-                    poly = np.array([[v.x, v.y, 0] for v in vertices], dtype=np.float64)
-                    shifted_poly = np.roll(poly, shift=-1, axis=0)
-                    edge_sources.append(poly)
-                    edge_targets.append(shifted_poly)
+        if edge_sources == [] or edge_targets == []:
+            return (
+                np.array([[0, 0, 0]], dtype=np.float64),
+                np.array([[0, 0, 0]], dtype=np.float64),
+            )
+        edge_source = np.vstack(edge_sources)
+        edge_target = np.vstack(edge_targets)
+        edge_vectors = edge_target - edge_source
 
-            if edge_sources == [] or edge_targets == []:
-                return (
-                    np.array([[0, 0, 0]], dtype=np.float64),
-                    np.array([[0, 0, 0]], dtype=np.float64),
-                )
-            edge_source = np.vstack(edge_sources)
-            edge_target = np.vstack(edge_targets)
-            edge_vectors = edge_target - edge_source
+        _cached_terrain = edge_source, edge_vectors
 
-            _cached_edges[excluded_terrain_id] = edge_source, edge_vectors
+        return _cached_terrain
 
-        return _cached_edges[excluded_terrain_id]
+    @staticmethod
+    def _get_terrain_filter(gs: GameState, source_ent: int) -> NDArray[np.bool]:
+        if source_ent in _terrain_filter:
+            return _terrain_filter[source_ent]
+
+        filter: list[bool] = []
+        _cache_inside: dict[int, bool] = {}
+        for terrain_id in _cached_edge_ids:
+            if terrain_id not in _cache_inside:
+                if IntersectSystem.is_inside(gs, terrain_id, source_ent):
+                    _cache_inside[terrain_id] = False
+                else:
+                    _cache_inside[terrain_id] = True
+            filter.append(_cache_inside[terrain_id])
+        _terrain_filter[source_ent] = np.array(filter, dtype=np.bool)
+        return _terrain_filter[source_ent]
 
     @staticmethod
     def check(gs: GameState, source_ent: int, target_ent: int) -> bool:
 
-        # Ensure two points are float points for a single line vector
         source = gs.get_component(source_ent, Transform).position
         target = gs.get_component(target_ent, Transform).position
-
-        # Create a vector of edges
         edge_source, edge_vectors = NewLosSystem._get_poly(gs, source_ent)
+        filter = NewLosSystem._get_terrain_filter(gs, source_ent)
 
         return NewLosSystem._check(
             (source.x, source.y),
             (target.x, target.y),
             edge_source,
             edge_vectors,
+            filter,
         )
 
     @staticmethod
@@ -89,12 +100,16 @@ class NewLosSystem:
         target_pos: tuple[float, float],
         edge_source: NDArray[np.float64],
         edge_vectors: NDArray[np.float64],
+        filter: NDArray[np.bool],
     ) -> bool:
         # Explicitly tell numpy that we're working with 2d vectors with z=0
         source = np.array([source_pos[0], source_pos[1], 0], dtype=np.float64)
         target = np.array([target_pos[0], target_pos[1], 0], dtype=np.float64)
-
         line_vector = target - source
+
+        # Filter out terrains
+        edge_source = edge_source[filter]
+        edge_vectors = edge_vectors[filter]
 
         # Compute two parametric values t & u of intersect point
         line_cross_edge = np.cross(line_vector, edge_vectors)[:, 2]
