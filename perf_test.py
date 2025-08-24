6d4d2157050ff0707cb8ea299272d200a0d8db70
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-08-24 10:45:44 +0700

    #55 update requirements.txt

--- perf_test.py
+++ perf_test.py
@@ -34,15 +34,15 @@ def test() -> None:
     MoveSystem.move(gs, 9, Vec2(-50, -200))
 
 
-# from timeit import timeit
+from timeit import timeit
 
-# exec_time = timeit(test, number=1)
-# print(f"Execution time: {exec_time:.6f} seconds")
+exec_time = timeit(test, number=1)
+print(f"Execution time: {exec_time:.6f} seconds")
 
-import cProfile
-import pstats
+# import cProfile
+# import pstats
 
-cProfile.run("test()", sort="tottime", filename="perftest.txt")
-p = pstats.Stats("perftest.txt")
-p.sort_stats("tottime")
-p.print_stats(20)
+# cProfile.run("test()", sort="tottime", filename="perftest.txt")
+# p = pstats.Stats("perftest.txt")
+# p.sort_stats("tottime")
+# p.print_stats(20)
