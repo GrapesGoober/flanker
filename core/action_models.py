04231c70bbd378a398d62d6ecade1220e64e4110
grapes-laptop <88650896+GrapesGoober@users.noreply.github.com>
2025-09-28 09:50:36 +0700

    #113 add fire & assault action models

--- core/action_models.py
+++ core/action_models.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-from core.components import FireControls
+from core.components import AssaultControls, FireControls
 from core.utils.vec2 import Vec2
 
 
 @dataclass
 class MoveAction:
-    """Body a move action of a combat unit to a position."""
+    """A singular move action of a combat unit to a position."""
 
     unit_id: int
     to: Vec2
@@ -13,9 +13,25 @@ class MoveAction:
 
 @dataclass
 class GroupMoveAction:
-    """Body a group move action consisting of multiple move actions."""
+    """A group move action consisting of multiple move actions."""
 
     moves: list[MoveAction]
+
+
+@dataclass
+class AssaultAction:
+    """An assault action of attacker combat unit assaulting target unit."""
+
+    attacker_id: int
+    target_id: int
+
+
+@dataclass
+class FireAction:
+    """A fire action of attacker combat unit firing at target unit."""
+    
+    attacker_id: int
+    target_id: int
 
 
 @dataclass
@@ -31,3 +47,20 @@ class GroupMoveActionResult:
     """Result of a group move action as multiple singular move results."""
 
     moveActionLogs: list[MoveActionResult]
+
+
+@dataclass
+class FireActionResult:
+    """Result of a fire action as validity and fire action outcome."""
+
+    is_valid: bool
+    outcome: FireControls.Outcomes | None = None
+
+
+@dataclass
+class AssaultActionResult:
+    """Result of an assault action as validity, outcome, and any reactive fire."""
+
+    is_valid: bool
+    outcome: AssaultControls.Outcomes | None = None
+    reactive_fire_outcome: FireControls.Outcomes | None = None
