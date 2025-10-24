4038498b0010b7f7e805e29642cf01d9ad0b0249
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-10-24 20:59:55 +0700

    #111 add los polygon prototype script

--- core/systems/los_system.py
+++ core/systems/los_system.py
@@ -1,7 +1,21 @@
+from itertools import pairwise
+import math
+from typing import Iterable
 from core.components import CombatUnit, TerrainFeature, Transform
 from core.gamestate import GameState
 from core.systems.intersect_system import IntersectSystem
+from core.utils.linear_transform import LinearTransform
 from core.utils.vec2 import Vec2
+from dataclasses import dataclass
+
+
+@dataclass
+class Intersection:
+    """Represents intersection between line and terrain feature."""
+
+    point: Vec2
+    terrain: TerrainFeature
+    terrain_id: int
 
 
 class LosSystem:
@@ -34,3 +48,79 @@ class LosSystem:
             if passed_one_terrain:
                 return False
         return True
+
+    @staticmethod
+    def get_los_polygon(
+        gs: GameState,
+        spotter_pos: Vec2,
+        range: float = 300,
+    ) -> list[Vec2]:
+
+        verts: list[Vec2] = []
+        for _, terrain, transform in gs.query(TerrainFeature, Transform):
+            if (terrain.flag & TerrainFeature.Flag.OPAQUE) == 0:
+                continue
+            verts += LinearTransform.apply(terrain.vertices, transform)
+        sorted_verts = LosSystem.sort_by_angle(verts, spotter_pos)
+        visible_points: list[Vec2] = []
+        for vert in sorted_verts:
+            ray = (vert - spotter_pos).normalized() * range
+            intersects = list(
+                LosSystem.get(
+                    gs=gs,
+                    start=spotter_pos,
+                    end=spotter_pos + ray,
+                    mask=TerrainFeature.Flag.OPAQUE,
+                )
+            )
+            if len(intersects) != 0:
+                visible_points.append(intersects[0].point)
+            else:
+                visible_points.append(spotter_pos + ray)
+
+        return visible_points
+
+    @staticmethod
+    def sort_by_angle(verts: list[Vec2], spotter_pos: Vec2) -> list[Vec2]:
+        def angle_from_spotter(v: Vec2) -> float:
+            # Vector from spotter_pos to vertex
+            rel = v - spotter_pos
+            # Compute angle relative to +x axis (0 radians)
+            theta = math.atan2(rel.y, rel.x)
+            # Normalize to [0, 2π)
+            if theta < 0:
+                theta += 2 * math.pi
+            return theta
+
+        return sorted(verts, key=angle_from_spotter)
+
+    @staticmethod
+    def get(
+        gs: GameState, start: Vec2, end: Vec2, mask: int = -1
+    ) -> Iterable[Intersection]:
+        """Yields intersections between the line segment and terrain."""
+        for id, terrain, transform in gs.query(TerrainFeature, Transform):
+            if terrain.flag & mask:
+                vertices = LinearTransform.apply(terrain.vertices, transform)
+                if terrain.is_closed_loop:
+                    vertices.append(vertices[0])
+                for b1, b2 in pairwise(vertices):
+                    point = LosSystem._get_intersect(start, end, b1, b2)
+                    if point is not None:
+                        yield Intersection(point, terrain, id)
+
+    @staticmethod
+    def _get_intersect(
+        a1: Vec2, a2: Vec2, b1: Vec2, b2: Vec2, tol: float = 0.1
+    ) -> Vec2 | None:
+        """Return an (x, y) intersection point between 2 line segments."""
+        da = a2 - a1  # Delta first segment a
+        db = b2 - b1  # Delta second segment b
+        if (denom := da.cross(db)) == 0:
+            return None  # Lines are parallel
+        diff = b1 - a1
+        t = diff.cross(db) / denom  # t and a are parametric values for intersection,
+        u = diff.cross(da) / denom  # along the length of the vectors using deltas
+        if -tol <= t <= 1 + tol and -tol <= u <= 1 + tol:
+            return a1 + da * t  # Offset p1 point by ua to get final point
+        return None  # Intersection is outside the segments
---
+++ test_los.py
@@ -0,0 +1,38 @@
+from dataclasses import is_dataclass
+from inspect import isclass
+from typing import Any
+
+from matplotlib import pyplot as plt
+from backend.tag_components import TerrainTypeTag
+from core import components
+from core.gamestate import GameState
+from core.systems.los_system import LosSystem
+from core.utils.vec2 import Vec2
+
+
+def visualize_points(verts: list[Vec2]) -> None:
+    xs = [v.x for v in verts]
+    ys = [-v.y for v in verts]
+
+    plt.scatter(xs, ys)  # type: ignore
+    plt.plot(xs, ys, linestyle="-")  # type: ignore
+    plt.axis("equal")  # type: ignore
+    plt.show()  # type: ignore
+
+
+if __name__ == "__main__":
+
+    component_types: list[type[Any]] = []
+    for _, cls in vars(components).items():
+        if isclass(cls) and is_dataclass(cls):
+            component_types.append(cls)
+    component_types.append(TerrainTypeTag)
+
+    path = "./scenes/demo-simple.json"
+
+    with open(path, "r") as f:
+        gs = GameState.load(f.read(), component_types)
+
+    poly = LosSystem.get_los_polygon(gs, Vec2(0, 0))
+
+    visualize_points(poly)
