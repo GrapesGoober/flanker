08a2df08e699645c7e4eee3e59a491039582d4ad
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2025-12-15 11:49:04 +0700

    #111 refactor _njit_get_intersect to use consistent terminology

--- core/utils/intersect_getter.py
+++ core/utils/intersect_getter.py
@@ -48,9 +48,12 @@ class IntersectGetter:
             edge_vectors.append([delta.x, delta.y, 0.0])
 
         # Run the optimised intersect getter
+        # Note: Explicitly tell numpy that we're working with 2d vectors with z=0;
+        # this prevents np.cross from freaking out
+        line_vector = line[1] - line[0]
         intersections = IntersectGetter._njit_get_intersect(
-            start_pos=(line[0].x, line[0].y),
-            end_pos=(line[1].x, line[1].y),
+            line_vert=np.array([line[0].x, line[0].y, 0], dtype=np.float64),
+            line_vector=np.array([line_vector.x, line_vector.y, 0], dtype=np.float64),
             edge_verts=np.array(edges, dtype=np.float64),
             edge_vectors=np.array(edge_vectors, dtype=np.float64),
         )
@@ -63,29 +66,28 @@ class IntersectGetter:
     @staticmethod
     @njit
     def _njit_get_intersect(
-        start_pos: tuple[float, float],
-        end_pos: tuple[float, float],
+        line_vert: NDArray[np.float64],
+        line_vector: NDArray[np.float64],
         edge_verts: NDArray[np.float64],
         edge_vectors: NDArray[np.float64],
     ) -> NDArray[np.float64]:
-        """Returns NDArray of intersection coordinates (N x 2)."""
-        # Explicitly tell numpy that we're working with 2d vectors with z=0;
-        # this prevents np.cross from freaking out
-        start = np.array([start_pos[0], start_pos[1], 0], dtype=np.float64)
-        end = np.array([end_pos[0], end_pos[1], 0], dtype=np.float64)
-        line_vector = end - start
-
+        """
+        Computes all line segment intersections between a line and a set of edges
+        represented as linear Bezier curves L = start + t * vector.
+        Returns NDArray (N x 2) of intersection points.
+        Note: this returns duplicate co-located points if edges share a vertex.
+        """
         # Compute two parametric values t & u of intersect point
-        line_cross_edge = np.cross(line_vector, edge_vectors)[:, 2]
-        q1_p1 = edge_verts - start
-        t = np.cross(q1_p1, edge_vectors)[:, 2] / line_cross_edge
-        u = np.cross(q1_p1, line_vector)[:, 2] / line_cross_edge
+        denominator = np.cross(line_vector, edge_vectors)[:, 2]
+        q1_p1 = edge_verts - line_vert
+        t = np.cross(q1_p1, edge_vectors)[:, 2] / denominator
+        u = np.cross(q1_p1, line_vector)[:, 2] / denominator
 
         # Note: we're trying to check t and u values in bound [0, 1].
         # Since the cost of missing a intersect point (false negative) is too severe,
-        # the tolerance has to eagerly catch an intersect [0-TOL, 1+TOL]
+        # the tolerance has to greedily catch an intersect [0-TOL, 1+TOL].
         TOL = 1e-9
-        parallel = np.abs(line_cross_edge) <= TOL
+        parallel = np.abs(denominator) <= TOL
         intersect_mask = (
             (~parallel) & (t >= -TOL) & (t <= 1 + TOL) & (u >= -TOL) & (u <= 1 + TOL)
         )
@@ -93,5 +95,5 @@ class IntersectGetter:
         # Calculate intersection points using P = start + t * line_vector
         # Only slice [:2] to grab x and y, ignore z=0
         t_valid = t[intersect_mask]
-        intersection_points = start[:2] + t_valid[:, None] * line_vector[:2]
+        intersection_points = line_vert[:2] + t_valid[:, None] * line_vector[:2]
         return intersection_points
