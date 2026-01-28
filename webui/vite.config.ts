8174c87ffc617ab5a17e649b413e2323040da1f0
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-01-28 21:12:07 +0700

    #139 add VITE_WEBAPI_BASE_URL variable

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,11 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': 'http://localhost:8000',
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const apiUrl = env.VITE_WEBAPI_BASE_URL ?? 'localhost:8000';
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			proxy: {
+				'/api': {
+					target: `http://${apiUrl}`,
+					changeOrigin: true
+				}
+			}
 		}
-	}
+	};
 });
