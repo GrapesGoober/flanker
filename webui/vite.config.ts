9edff9d8b2a4691824dc0bb95b7e11d04513f3b7
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-01-29 11:18:22 +0700

    #139 add env VITE_PORT

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	const apiUrl = env.VITE_WEBAPI_URL ?? 'http://localhost:8000';
+	const port = env.VITE_PORT ?? '5173';
 
 	return {
 		plugins: [sveltekit()],
 		server: {
+			port: Number(port),
 			proxy: {
 				'/api': {
 					target: apiUrl
