52842f480dd74d29924f4848d73486fa7606013b
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-03-05 21:36:16 +0700

    #153 update experiments

--- scripts/run_ai_trial.py
+++ scripts/run_ai_trial.py
@@ -56,8 +56,8 @@ def run_trial(
 
 
 if __name__ == "__main__":
-    SCENE_NAME = "experiment-us-us"
+    SCENE_NAME = "experiment-w1-wg"
     SCENE_FILE = f"./scenes/{SCENE_NAME}.json"
     RECORD_FILE = f"./scripts/experiment_results/{SCENE_NAME}.csv"
     gs = initialize_game_state(path=SCENE_FILE)
-    run_trial(gs, RECORD_FILE, n=1)
+    run_trial(gs, RECORD_FILE, n=300)
