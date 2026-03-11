79931b5efb0607d5d6744b6d030f8ba21828810d
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-03-11 15:25:38 +0700

    #172 refactor visualization with 2 units

--- scripts/visualize_los.py
+++ scripts/visualize_los.py
@@ -10,6 +10,22 @@ from flanker_core.systems.los_system import LinearTransform, LosSystem
 from matplotlib import pyplot as plt
 
 
+def load_state(path: str) -> GameState:
+
+    component_types: list[type[Any]] = []
+    for _, cls in vars(components).items():
+        if isclass(cls) and is_dataclass(cls):
+            component_types.append(cls)
+
+    with open(path, "r") as f:
+        entities, id_counter = Serializer.deserialize(
+            json_data=f.read(),
+            component_types=component_types,
+        )
+        gs = GameState.load(entities, id_counter)
+    return gs
+
+
 def visualize_polygon(verts: list[Vec2], color: str = "C0") -> None:
     xs = [v.x for v in verts]
     ys = [-v.y for v in verts]
@@ -19,21 +35,7 @@ def visualize_polygon(verts: list[Vec2], color: str = "C0") -> None:
     plt.axis("equal")  # type: ignore
 
 
-if __name__ == "__main__":
-
-    component_types: list[type[Any]] = []
-    for _, cls in vars(components).items():
-        if isclass(cls) and is_dataclass(cls):
-            component_types.append(cls)
-
-    path = "./scenes/demo-simple.json"
-
-    with open(path, "r") as f:
-        entities, id_counter = Serializer.deserialize(
-            json_data=f.read(),
-            component_types=component_types,
-        )
-        gs = GameState.load(entities, id_counter)
+def draw_terrains(gs: GameState) -> None:
 
     for _, terrain, transform in gs.query(
         components.TerrainFeature,
@@ -43,11 +45,22 @@ if __name__ == "__main__":
         if terrain.is_closed_loop:
             vertices.append(vertices[0])
         visualize_polygon(vertices, color="C1")
-    unit_id = 10
+
+
+def draw_los(gs: GameState, unit_id: int) -> None:
     LosSystem.update_los_polygon(gs, unit_id)
     center = gs.get_component(unit_id, components.Transform).position
     poly = gs.get_component(unit_id, components.FireControls).los_polygon
     assert poly
     visualize_polygon(poly, color="C0")
     plt.scatter(center.x, -center.y, color="C0")  # type: ignore
+
+
+if __name__ == "__main__":
+
+    gs = load_state(path="./scenes/experiment-template.json")
+    draw_terrains(gs)
+
+    draw_los(gs, unit_id=10)
+    draw_los(gs, unit_id=11)
     plt.show()  # type: ignore
