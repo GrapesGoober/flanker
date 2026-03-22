c217ae020cb091e0acfa6a3f58a53649e802decb
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-03-22 21:54:29 +0700

    #173 refactor AI to UUID

--- flanker-ai/src/flanker_ai/states/waypoints_actions.py
+++ flanker-ai/src/flanker_ai/states/waypoints_actions.py
@@ -1,28 +1,29 @@
 from dataclasses import dataclass
+from uuid import UUID
 
 
 @dataclass
 class WaypointMoveAction:
-    unit_id: int
+    unit_id: UUID
     move_to_waypoint_id: int
 
 
 @dataclass
 class WaypointPivotAction:
-    unit_id: int
+    unit_id: UUID
     pivot_to_waypoint_id: int
 
 
 @dataclass
 class WaypointFireAction:
-    unit_id: int
-    target_id: int
+    unit_id: UUID
+    target_id: UUID
 
 
 @dataclass
 class WaypointAssaultAction:
-    unit_id: int
-    target_id: int
+    unit_id: UUID
+    target_id: UUID
 
 
 WaypointAction = (
