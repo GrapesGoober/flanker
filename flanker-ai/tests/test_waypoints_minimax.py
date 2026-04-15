8358ae1e63b1700f1bee74b925447a8d1cf98a08
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-04-15 11:08:54 +0700

    #183 add more pathing test case

--- flanker-ai/tests/test_waypoints_minimax.py
+++ flanker-ai/tests/test_waypoints_minimax.py
@@ -141,6 +141,8 @@ def test_waypoints_pathing(fixture: Fixture) -> None:
     assert rs.waypoints[5].movable_paths[8] == [5, 6, 0, 7, 8]
     assert rs.waypoints[3].movable_paths[7] == [3, 7]
     assert rs.waypoints[3].movable_paths[8] == [3, 7, 8]
+    assert rs.waypoints[3].movable_paths[4] == [3, 5, 6, 4]
+    assert rs.waypoints[2].movable_paths[1] == [2, 0, 1]
 
 
 def test_optimal_waypoint(fixture: Fixture) -> None:
