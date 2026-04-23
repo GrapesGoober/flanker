1ccd4be64a0fd0feab69e382c12c41c4151f920a
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-04-23 20:22:51 +0700

    #183 add has_los override

--- flanker-ai/src/flanker_ai/states/waypoints/waypoints_los_system.py
+++ flanker-ai/src/flanker_ai/states/waypoints/waypoints_los_system.py
@@ -1,12 +1,27 @@
 from typing import override
 
+from flanker_ai.states.waypoints.models import WaypointsGraphComponent
 from flanker_core.gamestate import GameState
 from flanker_core.models.vec2 import Vec2
 from flanker_core.systems.los_system import LosSystem
 
 
 class WaypointsLosSystem(LosSystem):
+
     @staticmethod
     @override
     def has_los(gs: GameState, spotter_pos: Vec2, target_pos: Vec2) -> bool:
-        raise NotImplementedError()
+
+        _, waypoints_component = gs.query(WaypointsGraphComponent)[0]
+        waypoints = waypoints_component.waypoints
+
+        spotter_waypoint = min(
+            waypoints.values(),
+            key=lambda waypoint: abs((spotter_pos - waypoint.position).length()),
+        )
+        target_waypoint_id = min(
+            waypoints.keys(),
+            key=lambda idx: abs((target_pos - waypoints[idx].position).length()),
+        )
+
+        return target_waypoint_id in spotter_waypoint.visible_nodes
