e8d25cbac76d3e68e34bb43839e786056d9f6e75
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-05-02 20:41:09 +0700

    #183 move unabstracted state into submodule

--- flanker-ai/src/flanker_ai/ai_agent.py
+++ flanker-ai/src/flanker_ai/ai_agent.py
@@ -19,7 +19,7 @@ from flanker_ai.i_representation_state import IRepresentationState
 from flanker_ai.policies.expectimax_policy import ExpectimaxPolicy
 from flanker_ai.policies.minimax_policy import MinimaxPolicy
 from flanker_ai.policies.random_heuristic_policy import RandomHeuristicPolicy
-from flanker_ai.states.unabstracted_state import UnabstractedState
+from flanker_ai.states.unabstracted.unabstracted_state import UnabstractedState
 from flanker_ai.states.waypoints.waypoints_state import WaypointsState
 from flanker_core.gamestate import GameState
 from flanker_core.models.components import InitiativeState
