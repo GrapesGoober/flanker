ba9c3009c0d5b9ef1ae434a8aedefc1826f9e029
Grapes <88650896+GrapesGoober@users.noreply.github.com>
2026-05-21 09:18:56 +0700

    #193 test squash merge conflict (#199)

---
+++ flanker-ai/src/flanker_ai/states/waypoints/waypoints_state_factory.py
@@ -0,0 +1,11 @@
+class WaypointsStateFactory:
+    """
+    Just a testing code for squash merging conflict.
+    """
+
+    @staticmethod
+    def create_state() -> None:
+        """
+        Does nothing. I wanna test what merge conflicts look like.
+        """
+        ...
