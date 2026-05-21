67a1a36f3c8a2ad3e21157684ecc5731c0c59050
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-05-21 09:49:33 +0700

    #197 revert removal of set_ai_waypoints_coordinates

--- webapi/__init__.py
+++ webapi/__init__.py
@@ -10,6 +10,7 @@ from webapi.combat_unit_service import CombatUnitService
 from webapi.logging_service import LoggingService
 from webapi.models import (
     ActionLog,
+    AiWaypointConfigRequest,
     AssaultActionRequest,
     CombatUnitsViewState,
     FireActionRequest,
@@ -129,6 +130,16 @@ async def ai_play(
     gs = scene_service.get_game_state(scene_name, game_id)
     exec_time = timeit(lambda: AiService.play_trial(gs), number=1)
     print(f"Execution time: {exec_time:.6f} seconds")
+
+
+@app.post("/api/{sceneName}/{gameId}/ai-config-waypoints")
+async def ai_config_waypoints(
+    scene_name: str = Path(..., alias="sceneName"),
+    game_id: int = Path(..., alias="gameId"),
+    body: AiWaypointConfigRequest = Body(...),
+) -> None:
+    gs = scene_service.get_game_state(scene_name, game_id)
+    AiService.set_ai_waypoints_coordinates(gs, body)
 
 
 @app.put("/api/{sceneName}/{gameId}/terrain")
