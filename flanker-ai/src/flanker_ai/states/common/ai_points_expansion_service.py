5ebf37284f4be7c8dc0361c536b7905b997bcd0c
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-06-08 19:04:54 +0700

    #206 remove weight-pruning for flag_waypoints

--- flanker-ai/src/flanker_ai/states/common/ai_points_expansion_service.py
+++ flanker-ai/src/flanker_ai/states/common/ai_points_expansion_service.py
@@ -62,13 +62,8 @@ class AiPointsExpansionService:
                 case PointsConfig.PolygonalExpansionConfig():
                     raise NotImplementedError()
                 case PointsConfig.FlagPruneConfig():
-                    flag_waypoints = (
-                        AiPointsExpansionService._prune_waypoints_by_weight(
-                            waypoints=waypoints,
-                            remaining_size=expansion_config.flag_size,
-                        )
-                    )
-                    # Include combat unit positions to help with smarter pruning
+                    # Use combat unit positions as flags
+                    flag_waypoints: list[Vec2] = []
                     if config.use_combat_unit_positions == True:
                         for _, _, transform in gs.query(CombatUnit, Transform):
                             flag_waypoints.append(transform.position)
