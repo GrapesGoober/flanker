33eaca19e11c4830268c381ac8557bd573238c1d
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-06-09 11:17:48 +0700

    #206 add new experiment file schema

--- scripts/run_ai_trial.py
+++ scripts/run_ai_trial.py
@@ -1,7 +1,7 @@
-import csv
 from copy import deepcopy
 from dataclasses import is_dataclass
 from inspect import isclass
+from pathlib import Path
 from typing import Any
 
 from flanker_ai.ai_agent import AiAgent, AiConfigComponent
@@ -11,6 +11,7 @@ from flanker_core.models import components
 from flanker_core.models.components import InitiativeState
 from flanker_core.serializer import Serializer
 from flanker_core.systems.register_systems import register_systems
+from pydantic import BaseModel
 
 
 def initialize_game_state(path: str) -> GameState:
@@ -34,31 +35,73 @@ def initialize_game_state(path: str) -> GameState:
     return gs
 
 
+class BatchTally(BaseModel):
+    trials: int
+    blue_wins: int
+    red_wins: int
+    draws: int
+    blue_config: AiConfigComponent
+    red_config: AiConfigComponent
+
+
+def get_current_tally(record_file: str) -> BatchTally:
+    if not Path(record_file).is_file():
+        blue_config: AiConfigComponent | None = None
+        red_config: AiConfigComponent | None = None
+        for _, config in gs.query(AiConfigComponent):
+            if config.faction == InitiativeState.Faction.BLUE:
+                blue_config = config
+            if config.faction == InitiativeState.Faction.RED:
+                red_config = config
+        if blue_config == None:
+            raise Exception("AI config is missing for BLUE.")
+        if red_config == None:
+            raise Exception("AI config is missing for RED.")
+
+        return BatchTally(
+            trials=0,
+            blue_wins=0,
+            red_wins=0,
+            draws=0,
+            blue_config=blue_config,
+            red_config=red_config,
+        )
+
+    with open(record_file, "r") as f:
+        return BatchTally.model_validate_json(f.read())
+
+
+def save_tally(record_file: str, tally: BatchTally) -> None:
+    with open(record_file, "w") as f:
+        f.write(tally.model_dump_json(indent=2))
+
+
 def run_trial(
     gs: GameState,
     record_file: str,
     n: int = 1,
 ) -> None:
-    with open(record_file, mode="w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["trial", "winner"])
 
     for i in range(n):
         print(f"Running Trial {i=}...")
         new_gs = deepcopy(gs)
         result = AiTrial.run_trial(new_gs)
-        with open(record_file, mode="a", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-            if result.winner == None:
-                writer.writerow([i, "DRAW"])
-            else:
-                print(f"Winner is {result.winner}")
-                writer.writerow([i, result.winner.value])
+        tally = get_current_tally(record_file)
+        tally.trials += 1
+        match result.winner:
+            case None:
+                tally.draws += 1
+            case InitiativeState.Faction.BLUE:
+                tally.blue_wins += 1
+            case InitiativeState.Faction.RED:
+                tally.red_wins += 1
+        save_tally(record_file, tally)
+        print(f"Trial finished with winner {result.winner}")
 
 
 if __name__ == "__main__":
     SCENE_NAME = "experiment-unabstracted"
     SCENE_FILE = f"./scenes/{SCENE_NAME}.json"
-    RECORD_FILE = f"./scripts/experiment_results/{SCENE_NAME}.csv"
+    RECORD_FILE = f"./scripts/experiment_results/{SCENE_NAME}.json"
     gs = initialize_game_state(path=SCENE_FILE)
-    run_trial(gs, RECORD_FILE, n=300)
+    run_trial(gs, RECORD_FILE, n=10)
