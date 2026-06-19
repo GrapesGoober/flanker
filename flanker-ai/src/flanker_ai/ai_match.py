eaad811596baacbf82f721253c4bf17b4f4b9282
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-06-19 20:57:52 +0700

    #239 add runtime early break

--- flanker-ai/src/flanker_ai/ai_match.py
+++ flanker-ai/src/flanker_ai/ai_match.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from time import perf_counter
 
 from flanker_ai.actions import ActionResult
 from flanker_ai.ai_agent import AiAgent
@@ -6,9 +7,12 @@ from flanker_core.gamestate import GameState
 from flanker_core.models.components import InitiativeState
 from flanker_core.systems.objective_system import ObjectiveSystem
 
+MAX_RUNTIME_SECONDS = 360
+
 
 @dataclass
 class AiMatchResult:
+    runtime: float
     action_results: list[ActionResult]
     winner: InitiativeState.Faction | None
     blue_search_size: int
@@ -41,8 +45,16 @@ class AiMatch:
 
         # Let two agents fight each other over and over
         action_results: list[ActionResult] = []
+        start_time = perf_counter()
         objective_system = gs.get(ObjectiveSystem)
         while (winner := objective_system.get_winning_faction(gs)) == None:
+
+            # Max safety time limit per run so it doesn't consume CPU.
+            runtime = perf_counter() - start_time
+            if runtime > MAX_RUNTIME_SECONDS:
+                break
+
+            # Have the AI play agianst each other.
             blue_action_results = blue_agent.play_initiative(
                 callback=count_blue_search_size,
             )
@@ -55,7 +67,9 @@ class AiMatch:
             if not red_action_results and not blue_action_results:
                 break
 
+        runtime = perf_counter() - start_time
         return AiMatchResult(
+            runtime=runtime,
             action_results=action_results,
             winner=winner,
             blue_search_size=blue_search_size,
