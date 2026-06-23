7de456a8c5163c01e6e778e44e35658716eb15d2
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-06-23 20:15:53 +0700

    #245 add basic search size getter

---
+++ scripts/visualize_searchsize.py
@@ -0,0 +1,70 @@
+from itertools import product
+
+import matplotlib.pyplot as plt
+from flanker_ai.ai_agent import AiConfigComponent
+from flanker_core.models.components import InitiativeState
+from numpy import average
+from pydantic import BaseModel
+
+
+class MatchResult(BaseModel):
+    winner: InitiativeState.Faction | None
+    total_runtime: float
+    blue_search_sizes: list[int]
+    red_search_sizes: list[int]
+
+
+class ExperimentResult(BaseModel):
+    n_matches: int
+    blue_config: AiConfigComponent
+    red_config: AiConfigComponent
+    match_results: list[MatchResult]
+
+
+def main() -> None:
+
+    size = get_search_sizes(
+        scene_name="scene-1",
+        faction=InitiativeState.Faction.BLUE,
+        blue_configs=["analysis"],
+        red_configs=["grid", "analysis", "rh"],
+    )
+    print(size)
+
+    # plt.tight_layout()
+    # plt.savefig("scene-1.png", bbox_inches="tight")  # type: ignore
+
+
+def get_results(
+    experiment_name: str,
+) -> ExperimentResult:
+    file_path = f"./scripts/experiment_results/{experiment_name}.json"
+    with open(file_path, "r") as f:
+        file_data = f.read()
+        if file_data == "":
+            raise Exception(f"{file_path} file fmpty?!")
+        return ExperimentResult.model_validate_json(file_data)
+
+
+def get_search_sizes(
+    scene_name: str,
+    faction: InitiativeState.Faction,
+    blue_configs: list[str],
+    red_configs: list[str],
+) -> float:
+    # Just a placeholder to return the average
+    search_sizes: list[int] = []
+    for blue_conf, red_conf in product(blue_configs, red_configs):
+        experiment_name = f"{scene_name}-blue-{blue_conf}-red-{red_conf}-experiment"
+        results = get_results(experiment_name)
+        for match_result in results.match_results:
+            match faction:
+                case InitiativeState.Faction.BLUE:
+                    search_sizes += match_result.blue_search_sizes
+                case InitiativeState.Faction.RED:
+                    search_sizes += match_result.red_search_sizes
+    return float(average(search_sizes))
+
+
+if __name__ == "__main__":
+    main()
