41b4978b5e3ea531a1ddd32e8a0cdccc4cb58244
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-06-30 20:40:17 +0700

    run red-grid to 400

--- scripts/run_ai_experiment.py
+++ scripts/run_ai_experiment.py
@@ -61,14 +61,14 @@ def main() -> None:
             "blue-rh": "./scenes/experiment-blue-rh.json",
         },
         red_configs={
-            "red-analysis": "./scenes/experiment-red-analysis.json",
+            # "red-analysis": "./scenes/experiment-red-analysis.json",
             "red-grid": "./scenes/experiment-red-grid.json",
-            "red-rh": "./scenes/experiment-red-rh.json",
+            # "red-rh": "./scenes/experiment-red-rh.json",
         },
         match_settings={
             "experiment": "./scenes/experiment-settings.json",
         },
-        n_matches=300,
+        n_matches=400,
         max_processes=14,
     )
     run_experiment_set(my_run)
