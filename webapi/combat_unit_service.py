ec62132c15e2bf1486b2e481229035707b1379ac
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-07-13 16:59:12 +0700

    #256 remove add_squad

--- webapi/combat_unit_service.py
+++ webapi/combat_unit_service.py
@@ -1,14 +1,10 @@
-from uuid import UUID
-
 from flanker_core.gamestate import GameState
 from flanker_core.models.components import (
     CombatUnit,
     FireControls,
     InitiativeState,
-    MoveControls,
     Transform,
 )
-from flanker_core.models.vec2 import Vec2
 from flanker_core.systems.fire_system import FireSystem
 from flanker_core.systems.initiative_system import InitiativeSystem
 from flanker_core.systems.objective_system import ObjectiveSystem
@@ -18,24 +14,6 @@ from webapi.models import CombatUnitsViewState, SquadModel
 
 class CombatUnitService:
     """Provides static methods to add and query combat units and factions."""
-
-    @staticmethod
-    def add_squad(
-        gs: GameState,
-        pos: Vec2,
-        command_id: UUID,
-        faction: InitiativeState.Faction,
-    ) -> UUID:
-        """Add a new squad to the game state for a given faction."""
-        return gs.add_entity(
-            Transform(position=pos),
-            MoveControls(),
-            CombatUnit(
-                command_id=command_id,
-                faction=faction,
-            ),
-            FireControls(),
-        )
 
     @staticmethod
     def get_units_view_state(gs: GameState) -> CombatUnitsViewState:
