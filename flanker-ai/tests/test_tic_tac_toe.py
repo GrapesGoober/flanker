e35ddb4c7220c216fd6af39c0b90f9d1a774a703
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-07-20 11:20:38 +0700

    #265 add tic tac toe parameterization

--- flanker-ai/tests/test_tic_tac_toe.py
+++ flanker-ai/tests/test_tic_tac_toe.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import pytest
 from flanker_ai.policies.mcts_policy import MctsPolicy
 from flanker_ai.policies.minimax_policy import MinimaxPolicy
@@ -31,7 +33,11 @@ def test_str_simple_board(fixture: TicTacToeState) -> None:
     assert str(fixture) == expected
 
 
-def test_minimax_move(fixture: TicTacToeState) -> None:
+@pytest.mark.parametrize("policy_type", ["Minimax", "MCTS"])
+def test_minimax_move(
+    fixture: TicTacToeState,
+    policy_type: Literal["Minimax", "MCTS"],
+) -> None:
     expected = "\n".join(
         [
             "X . O",
@@ -39,24 +45,13 @@ def test_minimax_move(fixture: TicTacToeState) -> None:
             "X . O",
         ]
     )
-    minimax = MinimaxPolicy[TicTacToeAction](depth=1)
-    action, _ = minimax.get_action(fixture)
-    assert action != None
-    _, new_state = fixture.get_branches(action)[0]
-    assert action == TicTacToeAction(row=1, column=2)
-    assert str(new_state) == expected
+    match policy_type:
+        case "Minimax":
+            policy = MinimaxPolicy[TicTacToeAction](depth=1)
+        case "MCTS":
+            policy = MctsPolicy[TicTacToeAction](max_iterations=10_000)
 
-
-def test_mcts_move(fixture: TicTacToeState) -> None:
-    expected = "\n".join(
-        [
-            "X . O",
-            ". X O",
-            "X . O",
-        ]
-    )
-    minimax = MctsPolicy[TicTacToeAction](max_iterations=100_000)
-    action, _ = minimax.get_action(fixture)
+    action, _ = policy.get_action(fixture)
     assert action != None
     _, new_state = fixture.get_branches(action)[0]
     assert action == TicTacToeAction(row=1, column=2)
