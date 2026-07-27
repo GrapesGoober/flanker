27251a952dc422f34804608045c5611a0e19a266
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-07-27 20:04:57 +0700

    #298 add transposition table for waypoints state

--- flanker-ai/src/flanker_ai/states/waypoints/waypoints_state.py
+++ flanker-ai/src/flanker_ai/states/waypoints/waypoints_state.py
@@ -8,6 +8,7 @@ from flanker_ai.states.common.ai_branch_abstraction_service import (
     AiBranchAbstractionService,
 )
 from flanker_ai.states.common.ai_branching_service import AiBranchingService
+from flanker_ai.states.common.ai_cached_reward_service import AiCachedRewardService
 from flanker_ai.states.waypoints.waypoints_graph import WaypointsGraph
 from flanker_ai.states.waypoints.waypoints_los_system_overrides import (
     WaypointsLosSystemOverrides,
@@ -216,6 +217,7 @@ class WaypointsState(IRepresentationState[Action]):
     ) -> None:
 
         self.gs = deepcopy(gs)
+        AiCachedRewardService.init_empty_table(self.gs)
         self.gs.add_entity(
             LosSystemOverrides.GetLosFromLine(
                 method=WaypointsLosSystemOverrides.get_los_from_line,
@@ -239,8 +241,10 @@ class WaypointsState(IRepresentationState[Action]):
 
     @property
     @override
-    def cached_reward(self) -> float | None: ...
+    def cached_reward(self) -> float | None:
+        return AiCachedRewardService.get_reward(self.gs)
 
     @cached_reward.setter
     @override
-    def cached_reward(self, value: float) -> None: ...
+    def cached_reward(self, value: float) -> None:
+        AiCachedRewardService.set_reward(self.gs, value)
