e5df9da75403f0b8863d376f10ba7ca7e52624cf
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-08-31 15:45:27 +0700

    #328 add local script to connect to webapi

---
+++ scripts/run_experiments.py
@@ -0,0 +1,38 @@
+import json
+
+import requests
+from pydantic import BaseModel
+
+
+class ExperimentConfig(BaseModel):
+    url: str
+
+
+def main() -> None:
+    config = get_config()
+
+    r = requests.get(f"{config.url}/api/scenes")
+    print(r.text)
+
+    # scenes = [
+    #     "experiment-settings",
+    #     "experiment-scene-1",
+    #     "experiment-blue-rh",
+    #     "experiment-red-rh",
+    # ]
+
+    # r = requests.get(f"{config.url}/api/scenes/json", params={"sceneNames": scenes})
+    # scene_data = r.text
+    # print(scene_data)
+
+    # r = requests.post(f"{config.url}/api/ai-play", data={"state": scene_data})
+    # print(r.text)
+
+
+def get_config() -> ExperimentConfig:
+    with open("./scripts/configs/experiment-config.json", "r") as f:
+        return ExperimentConfig(**json.loads(f.read()))
+
+
+if __name__ == "__main__":
+    main()
