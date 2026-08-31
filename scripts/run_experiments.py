7cefc0039b11899bbd2c0f6051e650ffcfb89bb7
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-08-31 16:15:54 +0700

    #328 add concurrent requests

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -1,4 +1,6 @@
 import json
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
 
 import requests
 from pydantic import BaseModel
@@ -6,6 +8,12 @@ from pydantic import BaseModel
 
 class ExperimentConfig(BaseModel):
     url: str
+
+
+@dataclass
+class AiPlayRequest:
+    config: ExperimentConfig
+    scene_data: str
 
 
 def main() -> None:
@@ -17,21 +25,43 @@ def main() -> None:
     scenes = [
         "experiment-settings",
         "experiment-scene-1",
-        "experiment-blue-rh",
-        "experiment-red-rh",
+        "experiment-blue-analysis",
+        "experiment-red-analysis",
     ]
 
     r = requests.get(f"{config.url}/api/scenes/json", params={"sceneNames": scenes})
     scene_data = r.json()
-    # print(scene_data)
 
-    r = requests.post(f"{config.url}/api/ai-play", data=scene_data)
-    print(r.json())
+    WORKERS = 10
+
+    requests_to_send = [
+        AiPlayRequest(
+            config=config,
+            scene_data=scene_data,
+        )
+        for _ in range(WORKERS)
+    ]
+
+    with ThreadPoolExecutor(max_workers=len(requests_to_send)) as executor:
+        for result in executor.map(
+            send_ai_play_request,
+            requests_to_send,
+        ):
+            print(result.json())
 
 
 def get_config() -> ExperimentConfig:
     with open("./scripts/configs/experiment-config.json", "r") as f:
         return ExperimentConfig(**json.loads(f.read()))
+
+
+def send_ai_play_request(request: AiPlayRequest) -> requests.Response:
+    r = requests.post(
+        f"{request.config.url}/api/ai-play",
+        data=request.scene_data,
+    )
+    r.raise_for_status()
+    return r
 
 
 if __name__ == "__main__":
