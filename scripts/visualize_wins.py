295cbe1af774123632ff8af0ae3113db55550a4f
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-09-03 12:20:04 +0700

    #305 clean up plotnine code

--- scripts/visualize_wins.py
+++ scripts/visualize_wins.py
@@ -1,7 +1,7 @@
 import json
 from itertools import product
 from pathlib import Path
-from typing import Any
+from typing import TypedDict
 
 import pandas as pd
 from experiment_models import ExperimentResult, ExperimentSetConfig
@@ -17,40 +17,62 @@ from plotnine import (
 )
 
 
-def main() -> None:
-    results_root_path = "./scripts/outputs/experiment-results/"
+class _WinRateCell(TypedDict):
+    scene: str
+    blue: str
+    red: str
+    win_rate: float
 
+
+def main() -> None:
+
+    # Retrieve the each experiment results
     experiment_set = get_config(
         config_path="./scripts/configs/experiment-config.json",
     )
-    experiment_results = {
-        name: get_experiment_result(name, results_root_path)
+    experiment_results_by_name = {
+        name: get_experiment_result(
+            experiment_name=name,
+            results_root_path="./scripts/outputs/experiment-results/",
+        )
         for name in get_experiment_names(experiment_set)
     }
 
     # Can only plot from one match settings
     match_setting = experiment_set.match_settings[0]
 
-    # Plot win rate for each scene
-    win_rates_list: list[Any] = []
-    for scene_name in experiment_set.scene_configs:
-        # Create a BLUE's win rate mapping for each BLUE and each RED pairing
-        win_rates: dict[str, dict[str, float]] = {
-            blue_config: {
-                red_config: get_win_rate(
-                    experiment_name="-".join(
-                        [scene_name, blue_config, red_config, match_setting],
-                    ),
-                    experiment_results=experiment_results,
-                )
-                for red_config in experiment_set.red_configs
-            }
-            for blue_config in experiment_set.blue_configs
+    # Generate a list of win rate for each cells
+    cells: list[_WinRateCell] = [
+        {
+            "scene": scene_name,
+            "blue": blue_config,
+            "red": red_config,
+            "win_rate": get_win_rate(
+                experiment_name="-".join(
+                    [scene_name, blue_config, red_config, match_setting],
+                ),
+                experiment_results_by_name=experiment_results_by_name,
+            ),
         }
-        win_rates_list.append(win_rates)
+        for scene_name in experiment_set.scene_configs
+        for blue_config in experiment_set.blue_configs
+        for red_config in experiment_set.red_configs
+    ]
 
-    plot = plot_win_rates(win_rates_list, experiment_set.scene_configs)
-    plot.draw(show=True)
+    # Plot those cells
+    df = pd.DataFrame(cells)
+    plot = (
+        ggplot(df, aes(x="red", y="blue", fill="win_rate"))
+        + geom_tile()
+        + geom_text(aes(label="win_rate"))
+        + scale_fill_cmap(limits=(0, 1))
+        + facet_wrap("~scene", nrow=1)
+        + labs(
+            x="RED configuration",
+            y="BLUE configuration",
+        )
+    )
+    plot.show()
 
 
 def get_config(config_path: str) -> ExperimentSetConfig:
@@ -90,9 +112,9 @@ def get_experiment_result(
 
 def get_win_rate(
     experiment_name: str,
-    experiment_results: dict[str, ExperimentResult],
+    experiment_results_by_name: dict[str, ExperimentResult],
 ) -> float:
-    experiment = experiment_results[experiment_name]
+    experiment = experiment_results_by_name[experiment_name]
 
     return (
         sum(
@@ -100,42 +122,6 @@ def get_win_rate(
             for result in experiment.match_results
         )
         / experiment.n_matches
-    )
-
-
-def plot_win_rates(
-    win_rates_list: list[dict[str, dict[str, float]]], titles: list[str]
-) -> ggplot:
-    rows: list[dict[str, Any]] = []
-
-    for win_rates, title in zip(win_rates_list, titles):
-        for blue, red_rates in win_rates.items():
-            for red, win_rate in red_rates.items():
-                rows.append(
-                    {
-                        "plot": title,
-                        "blue": blue,
-                        "red": red,
-                        "win_rate": win_rate,
-                    }
-                )
-
-    df = pd.DataFrame(rows)
-
-    return (
-        ggplot(df, aes(x="red", y="blue", fill="win_rate"))
-        + geom_tile()
-        + geom_text(
-            aes(label="win_rate"),
-        )
-        + scale_fill_cmap(
-            limits=(0, 1),
-        )
-        + facet_wrap("~plot", nrow=1)
-        + labs(
-            x="RED configuration",
-            y="BLUE configuration",
-        )
     )
 
 
