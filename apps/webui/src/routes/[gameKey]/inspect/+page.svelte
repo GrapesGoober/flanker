9421fb57225a9c2f3e77bcc55f474e2b2d8b986c
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-09-06 12:59:36 +0700

    #338 add inspect route

--- apps/webui/src/routes/[gameKey]/inspect/+page.svelte
+++ apps/webui/src/routes/[gameKey]/inspect/+page.svelte
@@ -5,43 +5,28 @@
 	Handles log navigation and map rendering.
 	*/
 	import { page } from '$app/state';
-	import { GetLogs, GetMapData, type ActionLog, type MapViewState } from '$lib/api';
+	import { GetMapData, type MapViewState } from '$lib/api';
 	import { SvgMap, TerrainLayer } from '$lib/components';
 	import { loadGameLocal } from '$lib/scenes-storage';
 	import { onMount } from 'svelte';
-	import LogViewLayer from './log-view-layer.svelte';
 
-	let logData: ActionLog[] = $state([]);
-	let index: number = $state(0);
 	let mapData: MapViewState = $state({
 		terrains: [],
 		boundary: []
 	});
-	let action: ActionLog | null = $derived(logData[index] ?? null);
 
 	/* Loads terrain and log data on mount. */
 	onMount(async () => {
 		const gameKey: string = page.params['gameKey'] as string;
 		const gameStateJson = loadGameLocal(gameKey);
 		mapData = await GetMapData(gameStateJson);
-		logData = await GetLogs(gameStateJson);
 	});
 </script>
 
-<!-- Map and log view layers for current log index -->
 {#snippet mapSvgSnippet()}
 	<TerrainLayer {mapData} />
-	<LogViewLayer {logData} {index}></LogViewLayer>
 {/snippet}
-<!-- Log index input for navigation -->
-index = <input type="number" class="number-input" bind:value={index} />
 
-<!-- Map SVG rendering -->
 <div>
 	<SvgMap svgSnippet={mapSvgSnippet} />
 </div>
-
-<!-- Display current action log details -->
-{#if action}
-	{JSON.stringify(action)}
-{/if}
