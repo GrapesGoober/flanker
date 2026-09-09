e137d4873f6f2f99c89cdbda65c7eb57489a6a85
grapesgoober <88650896+GrapesGoober@users.noreply.github.com>
2026-09-09 14:45:23 +0700

    #337 add fire effect text

--- apps/webui/src/lib/components/svg-icons/fire-effect-arrows.svelte
+++ apps/webui/src/lib/components/svg-icons/fire-effect-arrows.svelte
@@ -2,7 +2,6 @@
 	import type { FireEffect, Vec2 } from '$lib/api';
 	import Arrow from './arrow.svelte';
 
-	const arrowHeadOffset = 15;
 	type Props = {
 		positionA: Vec2;
 		positionB: Vec2;
@@ -10,14 +9,55 @@
 		fireEffectB: FireEffect | null;
 	};
 	let props: Props = $props();
+
+	const angle = $derived(
+		(Math.atan2(
+			props.positionB.y - props.positionA.y,
+			props.positionB.x - props.positionA.x
+		) *
+			180) /
+			Math.PI
+	);
+
+	const length = $derived(
+		Math.hypot(
+			props.positionB.x - props.positionA.x,
+			props.positionB.y - props.positionA.y
+		)
+	);
 </script>
 
-{#if props.fireEffectA != null}
-	<Arrow
-		start={props.positionA}
-		end={props.positionB}
-		headOffset={arrowHeadOffset}
-		shaftWidth={2.5}
-		headSize={5}
-	/>
-{/if}
+<svg>
+	{#if props.fireEffectA != null}
+		<Arrow
+			start={props.positionA}
+			end={props.positionB}
+			headOffset={10}
+			shaftWidth={2.5}
+			headSize={5}
+		/>
+		<g
+			transform="
+                translate({props.positionA.x}, {props.positionA.y}) 
+                rotate({angle})"
+		>
+			<g transform="translate({length / 3}, 0)">
+				<foreignObject
+					x={0}
+					y={-8}
+					width="160"
+					height="160"
+					class="fire-effect-text"
+				>
+					{props.fireEffectA} FIRE
+				</foreignObject>
+			</g>
+		</g>
+	{/if}
+</svg>
+
+<style lang="less">
+	.fire-effect-text {
+		font-size: 0.3em;
+	}
+</style>
